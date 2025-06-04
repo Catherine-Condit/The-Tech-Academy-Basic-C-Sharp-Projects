de05ef9701e26eaa258b1081108922cc891df492
Catie Condit <concat2024@gmail.com>
2025-06-04 14:02:01 -0400

    Initial commit of Methods and Objects submission assignment.

---
+++ MethodsAndObjects/MethodsAndObjects/MethodsAndObjects/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Employee : Person // Inherits from Person class
+    {
+        public int Id { get; set; } // Unique identifier for the employee
+
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Person 
+    {
+        public string FirstName { get; set; } // Set the first name of the person
+        public string LastName { get; set; }// Set the last name of the person
+        public void SayName() // Method to display the full name of the person
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
