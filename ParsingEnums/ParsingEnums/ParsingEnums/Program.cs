2e3b034fdf0c9ccfd0640519b13a44970ec94d04
Catie Condit <concat2024@gmail.com>
2025-06-09 11:14:12 -0400

    Initial commit of Parsing Enums submission assignment.

---
+++ ParsingEnums/ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt the user to enter a day of the week
+            Console.WriteLine("Please enter the current day of the week:");
+            //read user input as a string
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                //try to parse input string into DaysOfWEek enum
+                //the 'true' parameter makes the parse case-insensitive
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput, true);
+
+                //if successful, print the successfully parsed enum value
+                Console.WriteLine($"You entered: {currentDay}");
+            }
+            catch (Exception)
+            {
+                //if parsing fails, catch the error and print friendly message
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            //wait for user input before closing the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+    //enum that represents the days of the week
+    public enum DaysOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
