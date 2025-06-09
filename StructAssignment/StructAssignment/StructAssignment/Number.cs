d54bf72a10779a9978f080f7f0aa0b9b5cc1df2d
Catie Condit <concat2024@gmail.com>
2025-06-09 12:10:09 -0400

    Initial commit of Struct Assignment.

---
+++ StructAssignment/StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    //define struct called Number
+    public struct Number
+    {
+        //give the struct a property "Amount" of data type decimal
+        public decimal Amount { get; set; }
+    }
+}
---
+++ StructAssignment/StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of data type Number 
+            Number myNumber = new Number();
+            //assign a value to the Amount property
+            myNumber.Amount = 100.50m;
+            //print the value of the Amount property to the console
+            Console.WriteLine("The amount is: " + myNumber.Amount);
+        }
+    }
+}
