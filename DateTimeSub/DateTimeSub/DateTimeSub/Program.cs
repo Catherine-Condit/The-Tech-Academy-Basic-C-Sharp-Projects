692bf17fb185d0978378eb990b3b79cdd65cf980
Catie Condit <concat2024@gmail.com>
2025-06-11 12:05:30 -0400

    Initial commit of Date/Time submission assignment.

---
+++ DateTimeSub/DateTimeSub/DateTimeSub/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DateTimeSub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //prints current date and time to console
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"Current Date and Time: {now}");
+            //ask user for number
+            Console.Write("\nEnter a number: ");
+            //prints to the console the exact time it will be in X hours, X being the number the user entered
+            int hoursToAdd;
+            if (int.TryParse(Console.ReadLine(), out hoursToAdd))
+            {
+                DateTime futureTime = now.AddHours(hoursToAdd);
+                Console.WriteLine($"The time in {hoursToAdd} hours will be: {futureTime}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
