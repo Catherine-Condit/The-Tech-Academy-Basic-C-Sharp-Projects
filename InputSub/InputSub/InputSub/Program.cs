5ba8a0684446b3fefbb399ef6f8ce0e173a80769
Catie Condit <concat2024@gmail.com>
2025-06-11 11:39:14 -0400

    Initial commit of input submission assignment.

---
+++ InputSub/InputSub/InputSub/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace InputSub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for a number
+            Console.WriteLine("Please enter a number:");
+            //read user input from console
+            string input = Console.ReadLine();
+
+            //define the path to the log file
+            string filePath = @"C:\c#logs\log1.txt";
+
+            //log input to a file
+            using (StreamWriter file = new StreamWriter(filePath, true)) //true says to append some text to the file, not overwrite
+            {
+                file.WriteLine(input);
+            }
+
+            //read to user in console
+            Console.WriteLine("\nContents of the log file:");
+            string fileContents = File.ReadAllText(filePath);
+            Console.WriteLine(fileContents);
+
+            //wait for user to press a key before closing the console window
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
