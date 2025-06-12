fe4fae9d9e2cf8b060511ffea4f4d425e7e7511a
Catie Condit <concat2024@gmail.com>
2025-06-12 11:00:39 -0400

    Initial commit of Constructor Submission assignment.

---
+++ ConstructorSub/ConstructorSub/ConstructorSub/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSub
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        //default constructor
+        public Person(string name) : this(name, 30) //chain two constructors
+        {
+            //this constructor initializes the Name property and sets a default Age of 30.
+        }
+        //overloaded constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        //method to display person information
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+}
---
+++ ConstructorSub/ConstructorSub/ConstructorSub/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a const variable
+            const string greeting = "Welcome to the Constructor Demo.";
+            Console.WriteLine(greeting);
+
+            //create a variable using keyword "var"
+            var userName = "Alice";
+            //create instances of the Person class using different constructors. Here, the default constructor is used because no age has been provided.
+            Person person1 = new Person(userName);
+            //call the DisplayInfo method to show the person's information
+            person1.DisplayInfo();
+
+            //create another instance of the Person class with a different name and age. Here, the overloaded constructor is used.
+            Person person2 = new Person("George", 30);
+            person2.DisplayInfo();
+
+            //wait for user input before closing the console
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
