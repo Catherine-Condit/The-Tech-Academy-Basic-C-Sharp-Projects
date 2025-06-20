f29f6149611de74b84bf92b794f4e2bd12e8f6e6
Catie Condit <concat2024@gmail.com>
2025-06-20 14:08:34 -0400

    Initial commit of MyFirstAzureWebApp

---
+++ MyFirstAzureWebApp/MyFirstAzureWebApp/Pages/Time.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstAzureWebApp.Pages
+{
+    public class TimeModel : PageModel
+    {
+        private readonly ILogger<TimeModel> _logger;
+
+        public DateTime ServerTimeUtc { get; set; }
+
+        public TimeModel(ILogger<TimeModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ServerTimeUtc = DateTime.UtcNow;
+        }
+    }
+
+}
---
+++ MyFirstAzureWebApp/MyFirstAzureWebApp/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// liveClock
+export function startLiveClock(serverTimeIso, elementId) {
+    let serverTime = new Date(serverTimeIso);
+
+    function updateClock() {
+        document.getElementById(elementId).textContent = serverTime.toLocaleString();
+        serverTime.setSeconds(serverTime.getSeconds() + 1);
+    }
+
+    updateClock();
+    setInterval(updateClock, 1000);
+}
+
